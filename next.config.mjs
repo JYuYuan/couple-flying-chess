8d50a9d32a064a16e2d2555323689f633262a771
JYY <438162987@qq.com>
2025-09-04 17:31:16 +0800

    feat(all): 游戏优化

--- next.config.mjs
+++ next.config.mjs
@@ -4,19 +4,6 @@ import runtimeCaching from 'next-pwa/cache.js';
 
 // 自定义缓存策略
 const customRuntimeCaching = [
-  // 图片缓存 7 天
-  {
-    urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/i,
-    handler: 'CacheFirst',
-    options: {
-      cacheName: 'image-cache',
-      expiration: {
-        maxEntries: 100,
-        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 天
-      },
-    },
-  },
-  // 音频缓存 30 天
   {
     urlPattern: /\.(?:mp3|wav|ogg)$/i,
     handler: 'CacheFirst',
